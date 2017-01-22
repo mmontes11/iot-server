8a5fb8ab0f20e7a62a82caf45ad814a5f02b951b
Martín Montes Pérez <martin.montes@nomasystems.com>
2017-01-22 18:16:01 +0100

    Typo fixed

--- config/env/production.js
+++ config/env/production.js
@@ -3,7 +3,8 @@ export default {
     db: 'mongodb://mongo_docker/js_backend',
     port: 8080,
     basicAuthUsers: {
-        'admin': process.env.BASIC_AUTH_PASWORD
+        'admin': process.env.BASIC_AUTH_PASSWORD
+
     },
     jwtSecret: process.env.JWT_SECRET
 };
