a086d4f293a120c798171ccab0a0e92396dce91d
Martín Montes Pérez <martin.montes@nomasystems.com>
2017-09-09 13:03:13 +0200

    Simplified TimePeriod class

--- src/models/request/timePeriod.js
+++ src/models/request/timePeriod.js
@@ -1,36 +1,27 @@
-import Enum from "es6-enum";
 import moment from "moment";
 import _ from "underscore";
 
 const dayString = "day", weekString = "week", monthString = "month", yearString = "year";
-const TIME_PERIOD = Enum(dayString, weekString, monthString, yearString);
+const validTimePeriods = [dayString, weekString, monthString, yearString];
 const supportedDateFormats = [moment.ISO_8601, "MM-DD-YYYY"];
 
 class TimePeriod {
     constructor(timePeriodString){
         if (!_.isUndefined(timePeriodString)) {
-            this.name = timePeriodString;
-            this.value = TimePeriod._stringToEnum(timePeriodString);
+            this.name = TimePeriod._normalizeTimePeriod(timePeriodString);
             this.startDate = moment().utc().subtract(1, this.name);
         }
         this.endDate = moment().utc();
     }
-    static _stringToEnum(timePeriodString) {
-        switch (timePeriodString) {
-            case dayString:
-                return TIME_PERIOD.day;
-            case weekString:
-                return TIME_PERIOD.week;
-            case monthString:
-                return TIME_PERIOD.month;
-            case yearString:
-                return TIME_PERIOD.year;
-            default:
-                return undefined
+    static _normalizeTimePeriod(timePeriodString) {
+        if (validTimePeriods.includes(timePeriodString)) {
+            return timePeriodString;
+        } else {
+            return undefined
         }
     }
     isValid() {
-        return !_.isUndefined(this.name) && !_.isUndefined(this.value)
+        return !_.isUndefined(this.name);
     }
 }
 
