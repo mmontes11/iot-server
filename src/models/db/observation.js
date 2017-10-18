fac0d13779626129c43409bfbd8a3469b9fc5f38
mmontes11 <martin11lrx@gmail.com>
2017-10-18 20:27:54 +0200

    RelatedEntitySchema moved to observation file

--- src/models/db/observation.js
+++ src/models/db/observation.js
@@ -1,6 +1,11 @@
 import _ from 'underscore';
 import mongoose from '../../../lib/mongoose';
-import { RelatedEntitySchema } from './relatedEntity';
+
+const RelatedEntitySchema = new mongoose.Schema({
+    name: String,
+    type: String,
+    geometry: mongoose.Schema.Types.GeoJSON
+});
 
 const ObservationSchema = new mongoose.Schema({
     username: String,
