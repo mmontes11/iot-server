c2542bb930ab718b93098fd91e8d6d0ff93bd968
mmontes11 <martin11lrx@gmail.com>
2017-12-20 20:00:44 +0100

    Geocoder implemented

---
+++ src/utils/geocoder.js
@@ -0,0 +1,38 @@
+import googleMaps from '../../lib/googleMaps';
+import _ from 'underscore';
+import { logInfo, logError } from './log';
+
+const geocode = async (address) => {
+    return new Promise((resolve, reject) => {
+        logInfo(`Geocoding address '${address}'...`);
+
+        googleMaps.geocode({
+            address: address
+        }, (err, response) => {
+            logInfo(`Geocoding response:`);
+            logInfo(JSON.stringify(response));
+            const httpStatus = response.status;
+
+            if (!_.isNull(err) || httpStatus >= 400) {
+                logError(`Geocoding error: HTTP ${httpStatus} status`);
+                logError(err);
+                reject(err);
+            } else {
+                const firstResult = _.first(response.json.results);
+                if (!_.isUndefined(firstResult)) {
+                    logInfo('Geocoding success');
+                    const location = firstResult.geometry.location;
+                    resolve({
+                        longitude: location.lng,
+                        latitude: location.lat
+                    });
+                } else {
+                    logInfo(`Geocoding: No results`);
+                    resolve(undefined);
+                }
+            }
+        });
+    });
+};
+
+export default { geocode };
