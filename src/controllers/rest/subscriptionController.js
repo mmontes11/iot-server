3c078084e37d7e25bd76675d7d47308938f89905
Martín Montes Pérez <martin.montes@nomasystems.com>
2018-04-28 20:07:30 +0200

    Fixed bug when creating subscription by topicId

--- src/controllers/rest/subscriptionController.js
+++ src/controllers/rest/subscriptionController.js
@@ -10,18 +10,20 @@ const createSubscription = async (req, res) => {
     const subscription = req.body;
     if (_.isUndefined(subscription.topic) && !_.isUndefined(subscription.topicId)) {
       const topic = await TopicModel.findTopicById(subscription.topicId);
+      if (_.isNull(topic)) {
+        return res.status(httpStatus.NOT_FOUND).json({ topicId: subscription.topicId });
+      }
       subscription.topic = topic.topic;
     }
     const foundSubscription = await SubscriptionModel.findSubscription(subscription);
     if (_.isNull(foundSubscription)) {
       const newSubscription = modelFactory.createSubscription(subscription);
       const savedSubscription = await newSubscription.save();
-      res.status(httpStatus.CREATED).json(savedSubscription);
-    } else {
-      res.status(httpStatus.CONFLICT).json(foundSubscription);
+      return res.status(httpStatus.CREATED).json(savedSubscription);
     }
+    return res.status(httpStatus.CONFLICT).json(foundSubscription);
   } catch (err) {
-    responseHandler.handleError(res, err);
+    return responseHandler.handleError(res, err);
   }
 };
 
