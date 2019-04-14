9f74a9bdd492fe74e0b3578b9e37d2828b17fa21
mmontes11 <martin11lrx@gmail.com>
2019-04-14 17:15:05 +0200

    Not sending logs to mongo, never

--- src/lib/winston.js
+++ src/lib/winston.js
@@ -1,28 +1,4 @@
 import { createLogger, format, transports } from "winston";
-import "winston-mongodb";
-import config from "../config";
-
-const loggerTransports = [
-  new transports.Console(),
-  new transports.File({
-    filename: "log-iot-server.log",
-  }),
-];
-
-if (config.storeLogsInMongo) {
-  loggerTransports.push(
-    new transports.MongoDB({
-      timestamp: true,
-      json: true,
-      colorize: true,
-      db: config.mongoUrl,
-      collection: "log-iot-server",
-      options: {
-        useNewUrlParser: true,
-      },
-    }),
-  );
-}
 
 const logger = createLogger({
   level: "info",
@@ -30,7 +6,12 @@ const logger = createLogger({
     format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
     format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`),
   ),
-  transports: loggerTransports,
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: "log-iot-server.log",
+    }),
+  ],
 });
 
 export default logger;
