434661622af11f29df2200eb0fea6a3b93a038fb
mmontes11 <martin11lrx@gmail.com>
2019-04-18 13:50:45 +0200

    Obtaining thing from token handshake

--- src/controllers/socket/socketController.js
+++ src/controllers/socket/socketController.js
@@ -7,13 +7,14 @@ export class SocketController {
   listen() {
     this.io.on("connection", socket => {
       logInfo("New connection");
-
+      logInfo(`Thing: ${JSON.stringify(socket.thing)}`);
       socket.on("disconnect", () => {
         logInfo("New disconnection");
       });
     });
   }
   close() {
+    logInfo("Socket server stopped");
     Object.keys(this.io.sockets.sockets).forEach(s => {
       this.io.sockets.sockets[s].disconnect(true);
     });
